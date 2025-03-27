f605c6dd8e3a73b032247906bc4f0e4813fc2c11
Saiph <clement.bx@ik.me>
2025-03-27 22:09:28 +0100

    Fix inputs

--- flake.nix
+++ flake.nix
@@ -1,17 +1,14 @@
 {
   description = "Configuration NixOS de Saiph avec flakes";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
-
   outputs = { self, nixpkgs, nixpkgs-stable, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit inputs self; };  # Passe à la fois inputs et self
       modules = [
-        ({ config, pkgs, ... }: {
+        ({ config, pkgs, lib, ... }: {
           # Définir nixpkgs-stable comme overlay pour pouvoir l'utiliser partout
           nixpkgs.overlays = [
             (final: prev: {
