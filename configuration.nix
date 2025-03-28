4cc7c1bf8e1f8f432625a17b956b4025c7889fb0
Saiph <clement.bx@ik.me>
2025-03-28 22:09:31 +0100

    Add et conf Vivaldi

--- configuration.nix
+++ configuration.nix
@@ -1,29 +1,21 @@
 { config, pkgs, lib, modulesPath, ... }:
-
 {
   imports = [
     ./hardware-configuration.nix
   ];
-
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
   # Nom de la machine
   networking.hostName = "nixos";
-
   # Wireguard
   networking.firewall.checkReversePath = false;
-
   # Activer le réseau
   networking.networkmanager.enable = true;
-
   # Configuration du fuseau horaire
   time.timeZone = "Europe/Paris";
-
   # Configuration de la localisation
   i18n.defaultLocale = "fr_FR.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "fr_FR.UTF-8";
     LC_IDENTIFICATION = "fr_FR.UTF-8";
@@ -35,25 +27,19 @@
     LC_TELEPHONE = "fr_FR.UTF-8";
     LC_TIME = "fr_FR.UTF-8";
   };
-
   # Activer le serveur X11
   services.xserver.enable = true;
-
   # Activer KDE Plasma 6
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-
   # Configuration du clavier
   services.xserver.xkb = {
     layout = "fr";
     variant = "";
   };
-
   console.keyMap = "fr";
-
   # Activer l'impression
   services.printing.enable = true;
-
   # Configuration du son avec PipeWire
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -63,7 +49,6 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
   # Création de l'utilisateur "saiph"
   users.users.saiph = {
     isNormalUser = true;
@@ -75,33 +60,32 @@
       discord
       stable.filezilla
       git
-      vscode  # Maintenant disponible via l'overlay
+      vscode
+      (vivaldi.overrideAttrs (old: {
+        dontWrapQtApps = false;
+        dontPatchELF = true;
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
+          kdePackages.wrapQtAppsHook
+        ];
+      }))
     ];
   };
-
   # Connexion automatique de l'utilisateur
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "saiph";
-
   # Installer Firefox
   programs.firefox.enable = true;
-
   # Installer Steam
   programs.steam.enable = true;
-
   # Autoriser les paquets non libres
   nixpkgs.config.allowUnfree = true;
-
   # Ajouter des paquets globaux si besoin
   environment.systemPackages = with pkgs; [
     # vim  # Décommente si tu veux ajouter Vim
     # wget
   ];
-
   # Activer les flakes et Nix command
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   # Version de l'état du système (ne pas modifier après installation)
   system.stateVersion = "24.11";
-
 }
